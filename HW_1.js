ea98bd0356b4086d74742c0ca0dae49732b49302
Larishka17 <larina-vasilieva@mail.ru>
2022-06-28 14:52:14 +0300

    JS_1

---
+++ HW_1.js
@@ -0,0 +1,190 @@
+// 1. Create variable “item_1”
+let item_1;
+
+// 2. Assign the variable item_1 the number 5
+item_1 = 5;
+
+// 3. Print item_1 to console
+console.log(item_1);
+
+//4. Create variable “item_2”
+let item_2;
+
+// 5. Assign the variable item_2 the number 3
+item_2 = 3;
+
+// 6. Print item_2 to console
+console.log(item_2);
+
+// 7. Create variable “item_3”
+let item_3;
+
+// 8. Assign the item_3 variable the addition of item_1 and item_2
+item_3 = item_1 + item_2; 
+
+// 9. Print item_3 to console
+console.log(item_3);
+
+// 10. Create variable “item_4”
+let item_4; 
+
+//  11. Assign the string “Yolochka” to the variable item_4
+item_4 = "Yolochka";
+
+// 12. Print item_4 to console
+console.log(item_4);
+
+// 13. Print the addition of item_3 and item_4 to the console
+console.log(item_3 + item_4);
+
+// 14. Print to the console the multiplication of item_3 and item_4
+console.log(item_3 * item_4);
+
+// 15. Create variable “item_5”
+let item_5; 
+
+// 16. Assign variable item_5 to variable item_3
+item_5 = item_3;
+
+// 17. Create variable item_6
+let item_6;
+
+// 18. Create variable item_6_type
+let item_6_type;
+
+// 19. Assign item_6 variable value 15
+item_6 = 15;
+
+// 20. Assign item_6_type variable the type of item_6 variable
+item_6_type = typeof(item_6);
+
+// 21. Print the data type item_6 to the console as —— “item_6 == ” item_6, “item_6_type == ” item_6_type —— 
+console.log('item_6 == ' + item_6 + ' ' + 'item_6_type == ' + item_6_type);
+
+// 22. Create variable item_7 and convert item_6 to String in it
+let item_7 = String(item_6);
+
+// 23. Create variable item_7_type 
+let item_7_type;
+
+// 24. Assign item_7_type variable the type of item_7 variable
+item_7_type = typeof(item_7);
+
+//  25. Print item_7 datatype to console as —— “item_7 == ” item_7, “item_7_type == ” item_7_type ——
+console.log('item_7 == ' + item_7 + ' ' + 'item_7_type == ' + item_7_type);
+
+// 26. Create variable "age_1" and assign value 10 to it
+let age_1 = 10;
+
+// 27. Create variable "age_2" and assign value 18 to it
+let age_2 = 18;
+
+// 28. Create variable "age_3" and assign value 60 to it
+let age_3 = 60;
+
+// 29-33 [Create an if in which you will check the value of the variable age_1
+// If age_1 < age_2 print to console “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
+// If age_1 >=  age_2 и age_1 <  age_3, print to console “Welcome  !”
+// If age_1  >= age_3, print to console “Keep calm and look Culture channel”.
+// Otherwise print “Technical work” ]
+if (age_1 < age_2){
+  console.log('You don`t have access cause your age is ' + age_1 + ' It`s less then ' + age_2)
+} else if (age_1 >=  age_2 && age_1 <  age_3) {
+ console.log('Welcome!')
+ } else if (age_1 > age_3) {
+    console.log('Keep calm and look Culture channel')
+ } else {
+     console.log('Technical work')
+ };
+
+
+// HW_1* 
+// Tasks of varying difficulty
+// 1*:
+// Convert the written code in 26-33 points into a function that takes age as input.
+// example: const checkAge = function(age) {
+// Your transformations
+// }
+// Output to the console the result of the function with ages 17, 18, 61 
+const checkAge = function(age) {
+    let age_2 = 18;
+    let age_3 = 60;
+    if (age < age_2){
+        return 'You don`t have access cause your age is ' + age + ' It`s less then ' + age_2
+    } else if (age >=  age_2 && age <  age_3) {
+        return 'Welcome!'
+    } else if (age >= age_3) {
+        return 'Keep calm and look Culture channel'
+    } else {
+        return 'Technical work'
+    }
+};
+
+console.log(checkAge(17));
+
+console.log(checkAge(18));
+
+console.log(checkAge(61));
+
+// 2*:
+// Convert assignment 1* so that the data type is checked first in the function. And if it is not a Number, an error was thrown.
+const checkAge = function(age) {
+    let age_2 = 18;
+    let age_3 = 60;
+    if (typeof age != 'number') {
+        return 'Error!'
+    }
+    else if (age < age_2){
+        return 'You don`t have access cause your age is ' + age + ' It`s less then ' + age_2
+    } else if (age >=  age_2 && age <  age_3) {
+        return 'Welcome!'
+    } else if (age >= age_3) {
+        return 'Keep calm and look Culture channel'
+    } else {
+        return 'Technical work'
+    }
+};
+console.log (checkAge('5'));
+
+// 3**:
+// Convert 2* in such a way that the value '2' (the string containing ONLY NUMBER) is skipped, converted to number
+const checkAge = function(age) {
+    Number(age);
+    let age_2 = 18;
+    let age_3 = 60;
+    if (isNaN(age)) {
+        return 'Error!'
+    }
+    else if (age < age_2){
+        return 'You don`t have access cause your age is ' + age + ' It`s less then ' + age_2
+    } else if (age >=  age_2 && age <  age_3) {
+        return 'Welcome!'
+    } else if (age >= age_3) {
+        return 'Keep calm and look Culture channel'
+    } else {
+        return 'Technical work'
+    }
+};
+console.log (checkAge('2'))
+
+// 4***:
+// Convert task 3* in such a way that the age is entered using the prompt function in the linked layout
+const checkAge = function(age) {
+    age = prompt('Enter your age', )
+    Number(age);
+    let age_2 = 18;
+    let age_3 = 60;
+    if (isNaN(age)) {
+        return 'Error!'
+    }
+    else if (age < age_2){
+        return 'You don`t have access cause your age is ' + age + ' It`s less then ' + age_2
+    } else if (age >=  age_2 && age <  age_3) {
+        return 'Welcome!'
+    } else if (age >= age_3) {
+        return 'Keep calm and look Culture channel'
+    } else {
+        return 'Technical work'
+    }
+};
+checkAge()
